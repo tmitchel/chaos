e6c4c817fe3098943b33cd3fec0bdc6aaf708db7
tylerbmitchell <tylerbmitchell6@gmail.com>
2018-04-16 18:19:49 -0500

    update documentation

--- feigenbaum.go
+++ feigenbaum.go
@@ -90,10 +90,16 @@ func main() {
 
 }
 
+//////////////////////////////////////////////////////////
+// Purpose: Handle the printing when r is such that the //
+// system is not in the chaotic region                  //
+// Return: Nothing (Printing to console)                //
+//////////////////////////////////////////////////////////
 func chaos_print(input *[][]chan float64, n_iter *int, r_print, x0_print *float64) {
     r := int(*r_print * 1000)
     x0 := int((*x0_print * 100))
 
+    // print the difference between requested x0 and one nearby
     for i := 1; i < *n_iter; i++ {
         Xn := <-(*input)[r][x0]
         Xnp := <-(*input)[r][x0+1]
@@ -101,16 +107,23 @@ func chaos_print(input *[][]chan float64, n_iter *int, r_print, x0_print *float6
     }
 }
 
+////////////////////////////////////////////////////////////
+// Purpose: Handle the printing of values/convergence for //
+// r not such that the system is in the chaotic region    //
+// Return: Nothing (Printing to console)                  //
+////////////////////////////////////////////////////////////
 func conv_print(input *[][]chan float64, n_iter *int, r_print, x0_print *float64) {
     r := int(*r_print * 1000)
     x0 := int((*x0_print * 100))
 
+    // hard-code to compare with the book when r=2, x0=0.3
     if *r_print == 2. {
         for i := 0; i < *n_iter; i++ {
             current := <-(*input)[r][x0]
             fmt.Printf("Asympt: %6.4f; Xn = %6.4f with diff %6.4f after %v iterations\n", 0.5, current, math.Abs(current-0.5), i+1)
         }
     } else if *r_print == 3.2 {
+        // hard-code to compare with the book when r=3.2, x0=0.3
         for i := 0; i < *n_iter; i++ {
             current := <-(*input)[r][x0]
             if math.Abs(current-0.51304) < math.Abs(current-0.79946) {
@@ -120,13 +133,17 @@ func conv_print(input *[][]chan float64, n_iter *int, r_print, x0_print *float64
             }
         }
     } else {
+        // not sure where all the attractors are so just print values
         for i := 0; i < *n_iter; i++ {
             fmt.Printf("Xn = %6.4f after %v iterations", <-(*input)[r][x0], i+1)
         }
     }
 }
 
-
+/////////////////////////////////////////////////////////////////
+// Purpose: Handle producing the pdf of the Feigenbaum Diagram //
+// Return: Nothing (pdf saved to system)                       //
+/////////////////////////////////////////////////////////////////
 func do_plotting(input *[][]chan float64, n *int) {
     // format data to be plotted
     total_len := len(*input) * len((*input)[0])
