c69b41cfbf6624a4f0d6deec39598fab1351cc08
tylerbmitchell <tylerbmitchell6@gmail.com>
2018-04-17 09:11:36 -0500

    prepare for switch to data struct

--- feigenbaum.go
+++ feigenbaum.go
@@ -90,56 +90,6 @@ func main() {
 
 }
 
-//////////////////////////////////////////////////////////
-// Purpose: Handle the printing when r is such that the //
-// system is not in the chaotic region                  //
-// Return: Nothing (Printing to console)                //
-//////////////////////////////////////////////////////////
-func chaos_print(input *[][]chan float64, n_iter *int, r_print, x0_print *float64) {
-    r := int(*r_print * 1000)
-    x0 := int((*x0_print * 100))
-
-    // print the difference between requested x0 and one nearby
-    for i := 1; i < *n_iter; i++ {
-        Xn := <-(*input)[r][x0]
-        Xnp := <-(*input)[r][x0+1]
-        fmt.Printf("Xn = %6.4f; Xn' = %6.4f; delta(X) = %6.4f\n", Xn, Xnp, math.Abs(Xn-Xnp))
-    }
-}
-
-////////////////////////////////////////////////////////////
-// Purpose: Handle the printing of values/convergence for //
-// r not such that the system is in the chaotic region    //
-// Return: Nothing (Printing to console)                  //
-////////////////////////////////////////////////////////////
-func conv_print(input *[][]chan float64, n_iter *int, r_print, x0_print *float64) {
-    r := int(*r_print * 1000)
-    x0 := int((*x0_print * 100))
-
-    // hard-code to compare with the book when r=2, x0=0.3
-    if *r_print == 2. {
-        for i := 0; i < *n_iter; i++ {
-            current := <-(*input)[r][x0]
-            fmt.Printf("Asympt: %6.4f; Xn = %6.4f with diff %6.4f after %v iterations\n", 0.5, current, math.Abs(current-0.5), i+1)
-        }
-    } else if *r_print == 3.2 {
-        // hard-code to compare with the book when r=3.2, x0=0.3
-        for i := 0; i < *n_iter; i++ {
-            current := <-(*input)[r][x0]
-            if math.Abs(current-0.51304) < math.Abs(current-0.79946) {
-                fmt.Printf("Asympt: %6.4f; Xn = %6.4f with diff %6.5f after %v iterations\n", 0.51304, current, math.Abs(current-0.51304), i+1)
-            } else {
-                fmt.Printf("Asympt: %6.4f; Xn = %6.4f with diff %6.5f after %v iterations\n", 0.79946, current, math.Abs(current-0.79946), i+1)
-            }
-        }
-    } else {
-        // not sure where all the attractors are so just print values
-        for i := 0; i < *n_iter; i++ {
-            fmt.Printf("Xn = %6.4f after %v iterations", <-(*input)[r][x0], i+1)
-        }
-    }
-}
-
 /////////////////////////////////////////////////////////////////
 // Purpose: Handle producing the pdf of the Feigenbaum Diagram //
 // Return: Nothing (pdf saved to system)                       //
@@ -190,3 +140,92 @@ func do_plotting(input *[][]chan float64, n *int) {
     p.Save(600, 400, "feigenbaum.pdf")
 }
 
+////////////////////////////////////////////////////////////
+// Purpose: Handle the printing of values/convergence for //
+// r not such that the system is in the chaotic region    //
+// Return: Nothing (Printing to console)                  //
+////////////////////////////////////////////////////////////
+func conv_print(input *[][]chan float64, n_iter *int, r_print, x0_print *float64) {
+    r := int(*r_print * 1000)
+    x0 := int((*x0_print * 100))
+
+    // hard-code to compare with the book when r=2, x0=0.3
+    if *r_print == 2. {
+        for i := 0; i < *n_iter; i++ {
+            current := <-(*input)[r][x0]
+            fmt.Printf("Asympt: %6.4f; Xn = %6.4f with diff %6.4f after %v iterations\n", 0.5, current, math.Abs(current-0.5), i+1)
+        }
+    } else if *r_print == 3.2 {
+        // hard-code to compare with the book when r=3.2, x0=0.3
+        for i := 0; i < *n_iter; i++ {
+            current := <-(*input)[r][x0]
+            if math.Abs(current-0.51304) < math.Abs(current-0.79946) {
+                fmt.Printf("Asympt: %6.4f; Xn = %6.4f with diff %6.5f after %v iterations\n", 0.51304, current, math.Abs(current-0.51304), i+1)
+            } else {
+                fmt.Printf("Asympt: %6.4f; Xn = %6.4f with diff %6.5f after %v iterations\n", 0.79946, current, math.Abs(current-0.79946), i+1)
+            }
+        }
+    } else {
+        // not sure where all the attractors are so just print values
+        for i := 0; i < *n_iter; i++ {
+            fmt.Printf("Xn = %6.4f after %v iterations", <-(*input)[r][x0], i+1)
+        }
+    }
+}
+
+//////////////////////////////////////////////////////////
+// Purpose: Handle the printing when r is such that the //
+// system is not in the chaotic region                  //
+// Return: Nothing (Printing to console)                //
+//////////////////////////////////////////////////////////
+func chaos_print(input *[][]chan float64, n_iter *int, r_print, x0_print *float64) {
+    r := int(*r_print * 1000)
+    x0 := int((*x0_print * 100))
+
+    // print the difference between requested x0 and one nearby
+    for i := 1; i < *n_iter; i++ {
+        Xn := <-(*input)[r][x0]
+        Xnp := <-(*input)[r][x0+1]
+        fmt.Printf("Xn = %6.4f; Xn' = %6.4f; delta(X) = %6.4f\n", Xn, Xnp, math.Abs(Xn-Xnp))
+    }
+}
+
+
+// TODO: put all of data into this struct to make accessing cleaner
+// TODO: make all current functions into methods of data_holder struct
+type data_holder struct {
+    r, x0 int
+    input *[][] chan float64
+}
+
+func (d data_holder) r_length() int {
+    return len(*d.input)
+}
+
+func (d data_holder) x_length() int {
+    return len((*d.input)[0])
+}
+
+func (d data_holder) get_r(r int) []chan float64 {
+    return (*d.input)[r*1000]
+}
+
+func (d data_holder) get(r, x int) chan float64 {
+    return (*d.input)[r*1000][x*1000]
+}
+
+func (d data_holder) get_iteration_n(n ...int) chan float64 {
+    var pop_chan chan float64
+    if len(n) == 1 {
+        pop_chan = (*d.input)[d.r*1000][d.x0*1000]
+    } else if len(n) == 3 {
+        pop_chan = (*d.input)[n[1]*1000][n[2]*1000]
+    } else {
+        fmt.Println("Wrong number of parameters. (Will add better handling later.)")
+        os.Exit(1)
+    }
+    for i := 0; i < n[0]; i++ {
+        <-pop_chan
+    }
+    return pop_chan
+}
