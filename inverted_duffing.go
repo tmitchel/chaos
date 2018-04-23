346773ddf1b079e17586f54ac0942141f30e729e
tylerbmitchell <tylerbmitchell6@gmail.com>
2018-04-23 09:02:33 -0500

    update documentation

--- inverted_duffing.go
+++ inverted_duffing.go
@@ -1,5 +1,13 @@
 package main 
 
+////////////////////////////////////////////////////
+// Purpose: To solve the Inverted Duffing         //
+// Oscillator given set of initial conditions and //
+// parameter values                               //
+// Return: A single pdf containing a 2D plot of x //
+// vs y (Vx)                                      //
+////////////////////////////////////////////////////
+
 import (
     // "fmt"
     "flag"
@@ -10,15 +18,28 @@ import (
     "gonum.org/v1/plot/plotter"
     )
 
+////////////////////////////////////////////////////
+// Purpose: Simple struct to hold a point for     //
+// plotting                                       //
+// Variables: X, Y coordinates and an int to      //
+// signal when a result blows up and can't be     //
+// plotted                                        //
+////////////////////////////////////////////////////
 type point struct {
     x, y float64
     breaker int
 }
 
+////////////////////////////////////////////////////
+// Purpose: Do the iterative calculations         //
+// Returns: A buffered channel packed with point  //
+// structs                                        //
+////////////////////////////////////////////////////
 func iter(x0, y0, F, dt float64) chan point {
     channel := make(chan point, 400)
 
     t0 := 0.
+    // the iteration is done here
     go func () {
         for x, y, t := x0, y0, t0; ; x, y, t = dt*y + x, dt*(F*math.Cos(t)-0.5*y+x-math.Pow(x, 3)) + y, t + dt {
             channel <- point{x: x, y: y}
@@ -29,6 +50,7 @@ func iter(x0, y0, F, dt float64) chan point {
 
 func main() {
 
+    // Command-line options
     F := flag.Float64("F", 0.24, "Constant F")
     x0 := flag.Float64("x0", 0, "Initial value for x")
     y0 := flag.Float64("y0", 0, "Initial value for y (dx/dt)")
@@ -37,7 +59,9 @@ func main() {
     max_min_comp := flag.Bool("comp", false, "Compare F=0.24 and F=0.35")
     flag.Parse()
 
+    // If true, plot highest F value vs lowest
     if *max_min_comp {
+        // channels holding high/low results
         low := iter(*x0, *y0, 0.24, 1./float64(*dt))
         high := iter(*x0, *y0, 0.35, 1./float64(*dt))
 
@@ -50,6 +74,7 @@ func main() {
         points_low := make(plotter.XYs, nsteps)
         points_high := make(plotter.XYs, nsteps)
 
+        // read from channels to fill plots
         for i := 0; i < nsteps; i++ {
             temp_low := <-low
             temp_high := <-high
@@ -62,6 +87,7 @@ func main() {
 
         }
 
+        // make the plots prettier
         plot_low, _ := plotter.NewLine(points_low)
         plot_low.Color = color.RGBA{R:255}
         plot_high, _ := plotter.NewLine(points_high)
@@ -75,9 +101,11 @@ func main() {
         p.Legend.Add("F=0.24", plot_low)
         p.Legend.Add("F=0.35", plot_high)
 
+        // save the pdf
         p.Save(600, 400, "iduff_comp.pdf")
 
     } else {
+        // channel to hold results for user chosen F value
         results := iter(*x0, *y0, *F, 1./float64(*dt))
 
         p, err := plot.New()
@@ -88,6 +116,7 @@ func main() {
         nsteps := (*t) * (*dt)
         points := make(plotter.XYs, nsteps)
 
+        // read from channel to fill plot
         for i := 0; i < nsteps; i++ {
             temp := <-results
 
